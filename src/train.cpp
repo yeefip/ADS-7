24c399b7206c4bc97047b29d2a922217deffd0f6
yeefip <yeefipp@gmail.com>
2025-05-15 20:46:28 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,56 +4,54 @@
 Train::Train() : operationCount(0), headCar(nullptr) {}
 
 Train::~Train() {
-  if (headCar) {
-    Car* currentCar = headCar;
-    do {
-      Car* tempCar = currentCar->next;
-      delete currentCar;
-      currentCar = tempCar;
-    } while (currentCar != headCar);
-  }
+    if (headCar) {
+        Car* currentCar = headCar;
+        do {
+            Car* tempCar = currentCar->next;
+            delete currentCar;
+            currentCar = tempCar;
+        } while (currentCar != headCar);
+    }
 }
 
 void Train::addCar(bool hasLight) {
-  Car* newCar = new Car{hasLight, nullptr, nullptr};
-  if (!headCar) {
-    headCar = newCar;
-    headCar->next = headCar;
-    headCar->prev = headCar;
-  } else {
-    Car* lastCar = headCar->prev;
-    lastCar->next = newCar;
-    newCar->prev = lastCar;
-    newCar->next = headCar;
-    headCar->prev = newCar;
-  }
+    Car* newCar = new Car{hasLight, nullptr, nullptr};
+    if (!headCar) {
+        headCar = newCar;
+        headCar->next = headCar;
+        headCar->prev = headCar;
+    } else {
+        Car* lastCar = headCar->prev;
+        lastCar->next = newCar;
+        newCar->prev = lastCar;
+        newCar->next = headCar;
+        headCar->prev = newCar;
+    }
 }
 
 int Train::getLength() {
-  operationCount = 0;
-  Car* currentCar;
-  while (true) {
-    currentCar = headCar;
-    int countedCars = 0;
-    if (!currentCar->light) {
-        currentCar->light = true;
+    operationCount = 0;
+    Car* currentCar;
+    while (true) {
+        currentCar = headCar;
+        int countedCars = 0;
+        if (!currentCar->light) {
+            currentCar->light = true;
+        }
+        currentCar = currentCar->next;
+        operationCount += 2;
+        while (!currentCar->light) {
+            currentCar = currentCar->next;
+            operationCount += 2;
+            countedCars++;
+        }
+        currentCar->light = false;
+        if (!headCar->light) {
+            return countedCars + 1;
+        }
     }
-    currentCar = currentCar->next;
-    operationCount += 2;
-    while (!currentCar->light) {
-      currentCar = currentCar->next;
-      operationCount += 2;
-      countedCars++;
-    }
-    currentCar->light = false;
-    if (!headCar->light) {
-      return countedCars + 1;
-    }
-  }
 }
 
 int Train::getOpCount() {
-  return operationCount;
-}
-    countOp = 0;
+    return operationCount;
 }
